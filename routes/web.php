a46b39822dd55e00e1ad588fc2c826374b3d315e
Pachara Tinamas <mrpachara@gmail.com>
2025-10-17 15:10:44 +0700

    feat(products): add product list

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +11,14 @@ Route::get('/', function () {
 Route::controller(HomeController::class)
     ->prefix('/homes')
     ->name('homes.')
+    ->group(static function(): void {
+        Route::get('', 'index')->name('index');
+    })
+;
+
+Route::controller(ProductController::class)
+    ->prefix('/products')
+    ->name('products.')
     ->group(static function(): void {
         Route::get('', 'index')->name('index');
     })
